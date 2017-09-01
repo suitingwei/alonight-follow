171a8e770b9cb667be3c8ff67b3046f6008ad6c6
sui <suitingwei@nanzhuxinyu.com>
2017-09-01 10:52:39 +0800

    Fix the misspell model name

--- src/AlonightFollowServiceProvider.php
+++ src/AlonightFollowServiceProvider.php
@@ -16,7 +16,7 @@ class AlonightFollowServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
 
         $this->publishes([
-            __DIR__ . '/Models/Followables.php'  => app_path('Models/Followables.php'),
+            __DIR__ . '/Models/Followable.php'  => app_path('Models/Followable.php'),
             __DIR__ . '/Models/Likeable.php'     => app_path('Models/Likeable.php'),
             __DIR__ . '/Models/Favorable.php'    => app_path('Models/Favorable.php'),
             __DIR__ . '/Models/Subscribable.php' => app_path('Models/Subscribable.php'),
